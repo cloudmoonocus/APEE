1a0cb99ce3b55b6477645123eb824c3864c4710c
cloudmoonocus <f356660222@foxmail.com>
2023-06-09 10:48:09 +0800

    add: 新增地图轨迹3,4

--- src/assets/data/path.js
+++ src/assets/data/path.js
@@ -1,19 +1,18 @@
 import { data as latlon1 } from './latlon/latlon1'
 import { data as latlon2 } from './latlon/latlon2'
+import { data as latlon3 } from './latlon/latlon3'
+import { data as latlon4 } from './latlon/latlon4'
 
-const path = [[], []]
-latlon1.forEach((item) => {
-	path[0].push({
-		time: item[0] + ' ' + item[1],
-		lng: item[3],
-		lat: item[2],
-	})
-})
-latlon2.forEach((item) => {
-	path[1].push({
-		time: item[0] + ' ' + item[1],
-		lng: item[3],
-		lat: item[2],
+const latlons = [latlon1, latlon2, latlon3, latlon4]
+const path = []
+latlons.forEach((latlon, index) => {
+	path[index] = []
+	latlon.forEach((item) => {
+		path[index].push({
+			time: item[0] + ' ' + item[1],
+			lng: item[3],
+			lat: item[2],
+		})
 	})
 })
 
